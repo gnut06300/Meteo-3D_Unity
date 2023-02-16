07f7c4aa022fbd60f8bd89345fe9590ba509e803
Gerald Col <gnut@gnut.eu>
2023-02-16 15:08:35 +0100

    First connect to API

--- Assets/Scripts/ApiRequest.cs
+++ Assets/Scripts/ApiRequest.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Networking;
 using UnityEngine;
 
 public class ApiRequest : MonoBehaviour
@@ -7,12 +8,39 @@ public class ApiRequest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // A correct website page.
+        StartCoroutine(GetRequest("https://api.openweathermap.org/data/2.5/weather?appid=20cc30210e1cd0dfe6f4d7dd7e3de6e5&lat=43.6961&lon=7.27178&units=metric&lang=fr"));
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator GetRequest(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            string[] pages = uri.Split('/');
+            int page = pages.Length - 1;
+
+            switch (webRequest.result)
+            {
+                case UnityWebRequest.Result.ConnectionError:
+                case UnityWebRequest.Result.DataProcessingError:
+                    Debug.LogError(pages[page] + ": Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.ProtocolError:
+                    Debug.LogError(pages[page] + ": HTTP Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.Success:
+                    Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
+                    break;
+            }
+        }
     }
 }
