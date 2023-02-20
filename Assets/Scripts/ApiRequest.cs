8b82b24539b772e36e61c69c51e074947f2b7576
gnut06300 <gnut@gnut.eu>
2023-02-20 16:48:44 +0100

    Setup Click and weather  4 days

--- Assets/Scripts/ApiRequest.cs
+++ Assets/Scripts/ApiRequest.cs
@@ -6,6 +6,7 @@ using Newtonsoft.Json;
 using System.IO;
 using TMPro;
 using UnityEngine.UI;
+using System;
 
 public class ApiRequest : MonoBehaviour
 {
@@ -16,19 +17,29 @@ public class ApiRequest : MonoBehaviour
     [SerializeField] TextMeshProUGUI cityHumidity;
     [SerializeField] TextMeshProUGUI cityState;
     private string key;
+
     // Start is called before the first frame update
     void Start()
     {
         key = File.ReadAllText(@"Assets\apikey.txt");
         // A correct website page.
-        string uri = "https://api.openweathermap.org/data/2.5/weather?appid=" + key + "&lat=43.6961&lon=7.27178&units=metric&lang=fr";
-        StartCoroutine(GetRequest(uri));
+        
+        /*string uri = "https://api.openweathermap.org/data/2.5/weather?appid=" + key + "&lat=43.6961&lon=7.27178&units=metric&lang=fr";
+        StartCoroutine(GetRequest(uri));*/
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void CallApi(Vector2 longLat)
+    {
+        string uri = "https://api.openweathermap.org/data/2.5/weather?appid=" + key + "&lat=" + longLat.y + "&lon=" + longLat.x + "&units=metric&lang=fr";
+        StartCoroutine(GetRequest(uri));
+        string uri4Days = "https://api.openweathermap.org/data/2.5/forecast?appid=" + key + "&lat=" + longLat.y + "&lon=" + longLat.x + "&units=metric&lang=fr";
+        StartCoroutine(GetRequest4Days(uri4Days));
     }
 
     IEnumerator GetRequest(string uri)
@@ -72,11 +83,55 @@ public class ApiRequest : MonoBehaviour
         }
     }
 
+    IEnumerator GetRequest4Days(string uri4Days)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri4Days))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            string[] pages = uri4Days.Split('/');
+            int page = pages.Length - 1;
+
+            switch (webRequest.result)
+            {
+                case UnityWebRequest.Result.ConnectionError:
+                case UnityWebRequest.Result.DataProcessingError:
+                    Debug.LogError(pages[page] + ": Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.ProtocolError:
+                    Debug.LogError(pages[page] + ": HTTP Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.Success:
+                    string json = webRequest.downloadHandler.text;
+                    Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
+                    WeatherReponse weatherReponse = JsonConvert.DeserializeObject<WeatherReponse>(json);
+                    
+                    break;
+            }
+        }
+    }
+    public class WeatherReponse4Days
+    {
+        public List<WeatherReponse> list { get; set; }
+    }
     public class WeatherReponse
     {
         public string name { get; set; } 
         public WeatherMain main { get; set; }
         public IList<Weather> weather { get; set; }
+        public DateTime dateMeteo { get; set; }
+
+        private string dt_txt;
+        public string Dt_txt
+        {
+            get { return dt_txt; }
+            set
+            {
+                dt_txt = value;
+                dateMeteo = DateTime.ParseExact(dt_txt, "yyyy-MM-dd HH:mm:ss", null);
+            }
+        }
     }
 
     public class WeatherMain
---
+++ Assets/Scripts/ClicLocation.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClicLocation : MonoBehaviour
+{
+    private Vector3 clickPos;
+    private bool clicked;
+    private Vector2 longLat;
+    [SerializeField] ApiRequest apiRequest;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnMouseDown()
+    {
+        clicked = Input.GetMouseButtonDown(0);
+        clickPos = Input.mousePosition;
+
+
+        RaycastHit rt;
+        Ray ray = Camera.main.ScreenPointToRay(clickPos);
+        if (Physics.Raycast(ray, out rt) && clicked)
+        {
+            Debug.Log("Coordonnées x, y, z: " + rt.point + " x :" + rt.point.x + " y :" + rt.point.y + " z :" + rt.point.z);
+            // nullIsland x :-0,9884224 y :-0,373946 z :-49,14286
+            Vector3 offset = rt.collider.transform.InverseTransformPoint(rt.point);
+            //Vector3 offset = rt.point;
+            longLat = ToSpherical(offset);
+
+            // appeler api request
+            apiRequest.CallApi(longLat);
+        }
+    }
+
+    public Vector2 ToSpherical(Vector3 position)
+    {
+        // Convert to a unit vector so our y coordinate is in the range -1...1.
+        position.Normalize();
+
+        // The vertical coordinate (y) varies as the sine of latitude, not the cosine.
+        float lat = Mathf.Asin(position.y) * Mathf.Rad2Deg;
+
+        // Use the 2-argument arctangent, which will correctly handle all four quadrants.
+        float lon = 90 - Mathf.Atan2(position.x, position.z) * Mathf.Rad2Deg;
+
+        // Here I'm assuming (0, 0, 1) = 0 degrees longitude, and (1, 0, 0) = +90.
+        // You can exchange/negate the components to get a different longitude convention.
+
+        Debug.Log(lat + ", " + lon);
+
+        // I usually put longitude first because I associate vector.x with "horizontal."
+        return new Vector2(lon, lat);
+    }
+}
